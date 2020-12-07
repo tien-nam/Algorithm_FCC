821a5a4baf958dd816e734b5eea2905d4455eef1
Cais <62311487+s3nzen@users.noreply.github.com>
2020-12-07 14:08:59 -0500

    Add hyperlink and script

--- Reponsive_Website_projects/survey_form/scripts/script.js
+++ Reponsive_Website_projects/survey_form/scripts/script.js
@@ -2,21 +2,27 @@ const formGroups = Array.from(document.querySelectorAll('.form-group'));
 let pageID = document.querySelector('#page_id');
 
 let page = 0;
-console.log(formGroups)
+
 
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
+const submitBtn = document.querySelector('#submit');
+const ticks = Array.from(document.querySelectorAll('#bg'));
 
-
-
+ticks.forEach(t => t.style.fill = "white");
+ticks[0].style.fill = "#6C63FF";
+console.log(ticks)
 
 prevBtn.addEventListener('click', e=>{
     page = (formGroups.length + --page) % formGroups.length;
     formGroups.forEach(g => g.classList.remove('invisibility'))
     formGroups[page].classList.add('invisibility');
+    //Update page 
     pageID.innerHTML = page + 1;
-    console.log({btn: 'prev' ,page, g:formGroups[page]})
-    console.log(nextBtn.disabled)
+    //update ticks on svg
+    ticks.forEach(t => t.style.fill = "white")
+    ticks[page].style.fill = "#6C63FF";
+    console.log(ticks[page])
 })
 
 nextBtn.addEventListener('click', e=>{
@@ -24,5 +30,11 @@ nextBtn.addEventListener('click', e=>{
     formGroups.forEach(g => g.classList.remove('invisibility'))
     formGroups[page].classList.add('invisibility');
     pageID.innerHTML = page + 1;
+    ticks.forEach(t => t.style.fill = "white")
+    ticks[page].style.fill = "#6C63FF";
     console.log({btn: 'next' ,page, g:formGroups[page]})
+})
+
+submitBtn.addEventListener('click', e => {
+    ticks.forEach(t => t.style.fill = "#6C63FF")
 })
